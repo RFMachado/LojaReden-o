5966094904e54aa55552cb57225f9aa1b118e61e
rafael <rafaellgremista@gmail.com>
2018-12-25 16:34:12 -0200

    Added splash screen.

---
+++ app/src/main/java/com/redencao/catalogo/catalogo/feature/splash/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.redencao.catalogo.catalogo.feature.splash
+
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import com.redencao.catalogo.catalogo.R
+import com.redencao.catalogo.catalogo.feature.main.ui.MainActivity
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            gotoMainActivity()
+        }, 700)
+    }
+
+    fun gotoMainActivity() {
+        val intent = MainActivity.launchIntent(this)
+        startActivity(intent)
+
+        finish()
+    }
+
+}

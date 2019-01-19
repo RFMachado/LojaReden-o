22fc058c9f346488f843f5c75ea64b256d812057
rafael <rafaellgremista@gmail.com>
2019-01-19 21:27:10 -0200

    Added size recyclerview.

--- app/src/main/java/com/redencao/catalogo/catalogo/feature/product/detail/ui/SizeAdapter.kt
+++ app/src/main/java/com/redencao/catalogo/catalogo/feature/product/detail/ui/SizeAdapter.kt
@@ -2,7 +2,6 @@ package com.redencao.catalogo.catalogo.feature.product.detail.ui
 
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.redencao.catalogo.catalogo.R
 import com.redencao.catalogo.catalogo.util.extensions.inflate
@@ -12,18 +11,28 @@ class SizeAdapter (items: Array<String>):
     RecyclerView.Adapter<SizeAdapter.ViewHolder>() {
 
     val sizes = items
-    var selectedColor = 0
+    var selectedPosition = RecyclerView.NO_POSITION
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int): Unit = with(holder.itemView) {
         val size = sizes[position]
 
+
         btnSize.text = size
 
         setOnClickListener {
-            btnSize.setTextColor(ContextCompat.getColor(context, R.color.gray))
-
-            selectedColor = position
+            notifyItemChanged(selectedPosition)
+            selectedPosition = position
+            notifyItemChanged(position)
         }
+
+        if(selectedPosition == position) {
+            btnSize.setBackgroundResource(R.drawable.black_stroke)
+        }
+        else {
+            btnSize.setBackgroundResource(R.drawable.circle_gray)
+        }
+
+
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
